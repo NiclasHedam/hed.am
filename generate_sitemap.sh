52ce59ade3d6099359085e3e17f6c39a264a4d59
Niclas Hedam <niclas@hed.am>
2020-10-11 00:22:37 +0200

    Removed newline in beginning of sitemap

--- generate_sitemap.sh
+++ generate_sitemap.sh
@@ -2,8 +2,7 @@
 
 ENTRIES=""
 
-echo '
-<?xml version="1.0" encoding="UTF-8"?>
+echo '<?xml version="1.0" encoding="UTF-8"?>
 <urlset
       xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
